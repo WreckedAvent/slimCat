879d64e7df600455fedcccf561c166fdb228ba8f
WreckedAvent <deliciousfaith@live.com>
2013-01-30 20:33:39 -0500

    derp

--- slimCat/System/Constants.cs
+++ slimCat/System/Constants.cs
@@ -9,7 +9,7 @@ namespace System
     {
         public const string CLIENT_ID = "slimCat";
         public const string CLIENT_NAME = "Caracal";
-        public const string CLIENT_VER = "b4.13";
+        public const string CLIENT_VER = "b4.14";
         public static string FRIENDLY_NAME = CLIENT_ID + ' ' + CLIENT_NAME + ' ' + CLIENT_VER;
     }
 }
