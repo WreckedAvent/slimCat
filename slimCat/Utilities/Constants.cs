25d6895c3eb3d0660241376bbee051a4ae85ae60
WreckedAvent <deliciousfaith@live.com>
2014-01-18 15:53:08 -0500

    3.05 release

--- slimCat/Utilities/Constants.cs
+++ slimCat/Utilities/Constants.cs
@@ -28,9 +28,9 @@ namespace Slimcat.Utilities
 
         public const string ClientId = "slimCat";
 
-        public const string ClientName = "ColorfulOcelot";
+        public const string ClientName = "Ocelot";
 
-        public const string ClientVer = "rc3.04 dev";
+        public const string ClientVer = "rc3.05";
 
         #endregion
 
