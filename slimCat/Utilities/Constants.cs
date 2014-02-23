c689403e46e5d5bdaa85497067003b2477ea13a0
WreckedAvent <deliciousfaith@live.com>
2014-02-23 18:42:44 -0500

    cutting 3.11

--- slimCat/Utilities/Constants.cs
+++ slimCat/Utilities/Constants.cs
@@ -39,7 +39,7 @@ namespace slimCat.Utilities
         /// <summary>
         ///     The client's version.
         /// </summary>
-        public const string ClientVer = "rc3.10";
+        public const string ClientVer = "rc3.11";
 
         /// <summary>
         ///     The endpoint for F-chat websocket communication.
