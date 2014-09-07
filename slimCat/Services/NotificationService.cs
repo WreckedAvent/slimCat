2dd0c7389288549384f037aac1992466e18dcba3
WreckedAdvent <deliciousfaith@live.com>
2014-09-07 10:19:43 -0400

    simplify some logic

--- slimCat/Services/NotificationService.cs
+++ slimCat/Services/NotificationService.cs
@@ -72,12 +72,9 @@ namespace slimCat.Services
             {
                 AddNotification =
                     notification => Dispatcher.Invoke((Action) (() => cm.Notifications.Backlog(notification, 100))),
-                ShowToast = () => toast.ShowNotifications(),
+                ShowToast = toast.ShowNotifications,
                 FlashWindow = () => Dispatcher.Invoke((Action) FlashWindow),
-                PlaySound = () => Dispatcher.Invoke((Action) (() =>
-                {
-                    if (ApplicationSettings.AllowSound) DingTheCrapOutOfTheUser();
-                })),
+                PlaySound = () => Dispatcher.Invoke((Action) DingTheCrapOutOfTheUser),
                 Toast = toast
             };
 
