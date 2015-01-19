381a4b65a8ad3a1d3ef7622ad1cfe7990066c740
WreckedAdvent <deliciousfaith@live.com>
2015-01-18 19:29:28 -0500

    cut 4.09

--- slimCat/Utilities/Constants.cs
+++ slimCat/Utilities/Constants.cs
@@ -39,7 +39,7 @@ namespace slimCat.Utilities
         /// </summary>
         public const string ClientName = "Puma";
 
-        public const double Version = 4.08;
+        public const double Version = 4.09;
 
         /// <summary>
         ///     The endpoint for F-chat websocket communication.
