79b19b879fa88612d5cd15db9a75b7f01afc759a
WreckedAdvent <deliciousfaith@live.com>
2015-03-31 18:14:33 -0400

    fix #335

--- slimCat/Commands/Character/LogInCommand.cs
+++ slimCat/Commands/Character/LogInCommand.cs
@@ -41,7 +41,8 @@ namespace slimCat.Models
         public override void DisplayNewToast(IChatState chatState, IManageToasts toastsManager)
         {
             if (!ApplicationSettings.ShowLoginToasts
-                || !chatState.IsInteresting(Model.TargetCharacter.Name))
+                || !chatState.IsInteresting(Model.TargetCharacter.Name)
+                || chatState.CharacterManager.IsOnList(Model.TargetCharacter.Name, ListKind.IgnoreUpdates, false))
             { return; }
 
             DoNormalToast(toastsManager);
