abc708085226ab0ce0ff996c8d877b90160ddc6b
WreckedAdvent <deliciousfaith@live.com>
2015-05-02 21:31:43 -0400

    add an excluded file from previous commit

---
+++ slimCat/Utilities/Converters/OneWayConverter.cs
@@ -0,0 +1,43 @@
+﻿#region Copyright
+
+// <copyright file="OneWayConverter.cs">
+//     Copyright (c) 2013-2015, Justin Kadrovach, All rights reserved.
+// 
+//     This source is subject to the Simplified BSD License.
+//     Please see the License.txt file for more information.
+//     All other rights reserved.
+// 
+//     THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+//     KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+//     IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+//     PARTICULAR PURPOSE.
+// </copyright>
+
+#endregion
+
+namespace slimCat.Utilities
+{
+    #region Usings
+
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    #endregion
+
+    /// <summary>
+    ///     Represents a converter which only converts to and not back.
+    /// </summary>
+    public abstract class OneWayConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => Convert(value, parameter);
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public abstract object Convert(object value, object parameter);
+    }
+}
