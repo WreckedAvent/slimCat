f23d211d13c002d8c5784aacb452ed3bb489e5e8
WreckedAdvent <deliciousfaith@live.com>
2015-05-03 10:33:13 -0400

    add shortcuts to build script

--- build.ps1
+++ build.ps1
@@ -6,5 +6,6 @@ copy-item .\Bootstrapper\bin\Release\Bootstrapper.exe .\build -recurse -Force
 rename-item .\build\client\slimCat.exe client.exe
 rename-item .\build\client\slimCat.pdb client.pdb
 rename-item .\build\client\slimCat.exe.config client.exe.config
-Move-Item .\build\client\lib\ .\build\
-Rename-Item .\build\Bootstrapper.exe slimCat.exe
+move-item .\build\client\lib\ .\build\
+rename-item .\build\Bootstrapper.exe slimCat.exe
+copy-item .\Shortcuts\* .\build\ -Force
