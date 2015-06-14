7ff03f6b6a16d8fafd4a3b34cd36b8fbd78ed2b5
WreckedAdvent <deliciousfaith@live.com>
2015-06-14 14:12:51 -0400

    expand meme

--- slimCat/Theme/EmbeddedTheme.xaml.cs
+++ slimCat/Theme/EmbeddedTheme.xaml.cs
@@ -2,11 +2,11 @@
 
 // <copyright file="EmbeddedTheme.xaml.cs">
 //     Copyright (c) 2013-2015, Justin Kadrovach, All rights reserved.
-// 
+//
 //     This source is subject to the Simplified BSD License.
 //     Please see the License.txt file for more information.
 //     All other rights reserved.
-// 
+//
 //     THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
 //     KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
 //     IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
@@ -37,6 +37,23 @@ namespace slimCat.Theme
         {
             e.Handled = true;
             sender.TryOpenRightClickMenuCommand<Grid>(2);
+        }
+
+        private void OnMemeExpand(object sender, MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+            var image = (Image)sender;
+
+            if (image.Height == 50)
+            {
+                image.Height = 100;
+                image.Width = 100;
+            }
+            else
+            {
+                image.Height = 50;
+                image.Width = 50;
+            }
         }
     }
 }
