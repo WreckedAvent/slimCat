2e832f23ba9bc40c20b112842e0b356e0856c324
WreckedAdvent <deliciousfaith@live.com>
2015-08-30 22:10:56 -0400

    version bump

--- slimCat/Utilities/Constants.cs
+++ slimCat/Utilities/Constants.cs
@@ -383,7 +383,7 @@ namespace slimCat.Utilities
         /// <summary>
         ///     The version of the client.
         /// </summary>
-        public const double Version = 5.003;
+        public const double Version = 5.004;
 
         /// <summary>
         ///     The endpoint for F-chat websocket communication.
