e4a746d4d275e7b09018d2fe6fbe08b44c27fac5
wreckedavent <deliciousfaith@live.com>
2016-08-27 13:35:32 -0400

    fix for #404 maybe probably

--- slimCat/Services/FlistService.cs
+++ slimCat/Services/FlistService.cs
@@ -2,11 +2,11 @@
 
 // <copyright file="FlistService.cs">
 //     Copyright (c) 2013-2015, Justin Kadrovach, All rights reserved.
-// 
+//
 //     This source is subject to the Simplified BSD License.
 //     Please see the License.txt file for more information.
 //     All other rights reserved.
-// 
+//
 //     THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
 //     KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
 //     IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
@@ -204,8 +204,6 @@ namespace slimCat.Services
                 return;
 
             var commandType = command["type"] as string;
-
-            command = new Dictionary<string, object>(command);
 
             switch (commandType)
             {
